72439383d4e068ea5d4e2f84531ecd3d94363638
Suad Wolgram <72341912+swolg@users.noreply.github.com>
2024-03-06 16:28:32 +0100

    Refactor code and remove unused components change speaker pos add ticket comp

--- src/components/pages/home/schedule/schedule.jsx
+++ src/components/pages/home/schedule/schedule.jsx
@@ -8,7 +8,7 @@ import illustration from './images/schedule.png';
 const TITLE = 'Schedule';
 
 const DESCRIPTION =
-  "Get ready for action-packed days. With two tracks and more than 45 sessions to choose from, you'll have plenty of opportunities to learn from experts in the field. The whole event will be held in English.";
+  "Get ready for action-packed days. With two tracks and more than 45 sessions to choose from, you'll have plenty of opportunities to learn from experts in the field.";
 
 const Schedule = () => (
   <section className="safe-paddings relative bg-white pb-40 lg:pb-32 md:py-24 sm:py-16">
@@ -21,18 +21,12 @@ const Schedule = () => (
           {TITLE}
         </h2>
         <p className="mt-5 max-w-[488px] text-lg leading-normal lg:max-w-[650px]">{DESCRIPTION}</p>
-        <Button
-          className="mt-7 text-white"
-          to="/schedule"
-          theme="blue"
-          size="lg"
-          target="_blank"
-        >
+        <Button className="mt-7 text-white" to="/schedule" theme="blue" size="lg" target="_blank">
           View full schedule
         </Button>
       </div>
       <img
-        className="mt-12 mr-16 h-[238px] w-[520px] xl:ml-10 lg:mx-auto lg:h-auto sm:max-h-[200px]"
+        className="mr-16 mt-12 h-[238px] w-[520px] xl:ml-10 lg:mx-auto lg:h-auto sm:max-h-[200px]"
         src={illustration}
         width="auto"
         height="auto"
