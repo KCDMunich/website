f078ac0e5e491df7ea37d44e5b8b058faab9b233
Suad Wolgram <72341912+swolg@users.noreply.github.com>
2024-11-18 15:17:27 +0100

    add gold sponsor

--- src/components/pages/home/sponsors/sponsors.jsx
+++ src/components/pages/home/sponsors/sponsors.jsx
@@ -1,81 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
-import { ExternalLink } from 'lucide-react';
 
-// Icon imports - Note: These would need to be handled by your application
-import adn from 'icons/adn_logo.png';
-import Akamai from 'icons/akamai.svg';
-import ApeFactory from 'icons/apefactory.webp';
-import Camptocamp from 'icons/camptocamp.svg';
-import cisco from 'icons/cisco.png';
-import Cloudnative from 'icons/cloudnative.svg';
-import consol from 'icons/consol.png';
-import csp2 from 'icons/csp2_claim_logo_V1.jpg';
-import Dgi from 'icons/Dgi.png';
-import dynatrace from 'icons/dynatrace.png';
-import Exoscale from 'icons/exoscale.webp';
-import germantech from 'icons/germantech.svg';
-import Isovalent from 'icons/isovalent.svg';
-import Kubecareers from 'icons/kubecareers_1.svg';
-import Kubeevents from 'icons/kubeevents_2.svg';
+import ApeFactory from 'icons/apefactory.svg';
+
 import liquid from 'icons/liquid.png';
-import Maiborn from 'icons/maiborn.jpeg';
-import MetalStack from 'icons/metalstackcloud.webp';
-import MindcurvGmbH from 'icons/Mindcurv_Logo_Color.svg';
-import paloalto from 'icons/paloalto.jpeg';
-import pulumi from 'icons/pulumi_logo.svg';
-import Qaware from 'icons/qaware.svg';
-import spectrocloud from 'icons/spectrocloud.svg';
-import Splunk from 'icons/Splunk_logo.svg';
-import StackState from 'icons/stackstate.png';
-import Steadforce from 'icons/steadforce_logo.png';
-import Syseleven from 'icons/syseleven.png';
-import Tigera from 'icons/Tigera_Logo_Logo.jpg';
-import veeam from 'icons/veeam.png';
+
 import whiteduck from 'icons/whiteduck.png';
 import './sponsor.css';
 
-const CARD_STYLES = 'w-[auto] h-[8vh]';
+const CARD_STYLES = 'w-[200px] h-[100px]';
 
 const SHOW_SPONSORS = true;
 
 const tierConfig = {
-  /* gold: {
+  gold: {
     title: 'Gold',
     class: 'bg-white shadow-sm',
     badgeClass: 'bg-yellow-50 text-yellow-500',
-    count: 6,
   },
   silver: {
     title: 'Silver',
     class: 'bg-white shadow-sm',
     badgeClass: 'bg-gray-50 text-gray-500',
-    count: 12,
   },
   bronze: {
     title: 'Bronze',
     class: 'bg-white shadow-sm',
-    badgeClass: 'bg-bronze-50 text-bronze-500',
-    count: 4,
-  }, */
+    badgeClass: 'bg-orange-50 text-orange-500',
+  },
   organizer: {
     title: 'Organizers',
     class: 'bg-white shadow-sm',
     badgeClass: 'bg-blue-50 text-blue-500',
-    count: 2,
   },
-  /* partner: {
+  partner: {
     title: 'Community & Media Partners',
     class: 'bg-white shadow-sm',
     badgeClass: 'bg-green-50 text-green-500',
-    count: 5,
-  }, */
+  },
 };
 
 const sponsorsList = [
   // Gold Sponsors
   { name: 'APE Factory', icon: ApeFactory, url: 'https://www.apefactory.com/de', tier: 'gold' },
+  /*
   { name: 'Cisco', icon: cisco, url: 'https://www.cisco.com/', tier: 'gold' },
   { name: 'Consol', icon: consol, url: 'https://www.consol.com/', tier: 'gold' },
   { name: 'MetalStack', icon: MetalStack, url: 'https://metalstack.cloud/de', tier: 'gold' },
@@ -107,10 +76,12 @@ const sponsorsList = [
   { name: 'Maiborn Wolff', icon: Maiborn, url: 'https://www.maibornwolff.de/', tier: 'bronze' },
   { name: 'DGi', icon: Dgi, url: 'https://www.dg-i.net/', tier: 'bronze' },
 
+  */
   // Organizers
   { name: 'Liquid Reply', icon: liquid, url: 'https://liquidreply.com/', tier: 'organizer' },
   { name: 'white duck', icon: whiteduck, url: 'https://whiteduck.de/', tier: 'organizer' },
 
+  /*
   // Community & Media Partners
   { name: 'Cloud Native', icon: Cloudnative, url: 'https://www.cncf.io/', tier: 'partner' },
   { name: 'Kube Events', icon: Kubeevents, url: 'https://kube.events/', tier: 'partner' },
@@ -122,6 +93,7 @@ const sponsorsList = [
     tier: 'partner',
   },
   { name: 'SysEleven', icon: Syseleven, url: 'https://www.syseleven.de/', tier: 'partner' },
+   */
 ];
 
 const Sponsors = ({
@@ -153,7 +125,7 @@ const Sponsors = ({
               <a
                 href={`mailto:${contactEmail}`}
                 className=" hover:underline"
-                style={{ color: '004258', fontWeight: 'bold' }}
+                style={{ color: '#004258', fontWeight: 'bold' }}
               >
                 {contactEmail}
               </a>{' '}
@@ -184,6 +156,9 @@ const Sponsors = ({
           >
             Sponsor Prospectus
           </a>
+          <p className="mb-8 text-lg text-gray-500" style={{ textAlign: 'center' }}>
+            Thank you to our amazing sponsors who make this event possible
+          </p>
         </div>
       </div>
 
@@ -194,11 +169,7 @@ const Sponsors = ({
 
         return (
           <div key={tier} className="mb-12">
-            <div className="mb-16 text-center">
-              <p className="mb-8 text-lg text-gray-500" style={{ textAlign: 'center' }}>
-                Thank you to our amazing sponsors who make this event possible
-              </p>
-            </div>
+            <div className="mb-16 text-center"></div>
             <div className="mb-6 flex items-center justify-center gap-2">
               <h3 className="text-xl font-semibold text-gray-900">{config.title}</h3>
               <span
@@ -223,14 +194,15 @@ const Sponsors = ({
                       config.class
                     )}
                   >
-                    <div className="relative h-full w-full">
+                    <div className="relative flex h-full w-full items-center justify-center">
                       <img
                         src={sponsor.icon}
                         alt={sponsor.name}
                         loading="lazy"
                         style={{
-                          height: '7vh',
-                          width: 'auto',
+                          maxHeight: '100%',
+                          maxWidth: '80%',
+                          objectFit: 'contain',
                         }}
                       />
                     </div>
