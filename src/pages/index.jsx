a1cfd6b9d496ca2de3275858267df5c594406e0c
Suad Wolgram <72341912+swolg@users.noreply.github.com>
2025-01-24 12:36:57 +0100

    delete imports

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,16 +2,13 @@
 import React from 'react';
 
 import Hero from 'components/pages/home/hero';
+import HotelList from 'components/pages/home/hotels';
 import Info from 'components/pages/home/info';
+import Proposal from 'components/pages/home/proposal';
 import Sponsors from 'components/pages/home/sponsors';
 import Venue from 'components/pages/home/venue';
-import HotelList from 'components/pages/home/hotels';
 import Layout from 'components/shared/layout';
 import SEO from 'components/shared/seo';
-import Proposal from 'components/pages/home/proposal';
-
-import GoldSponsor from 'components/pages/home/goldsponsor';
-import FloorPlan from 'components/pages/home/floorplan/floorplan';
 
 const HomePage = ({ location }) => (
   <Layout homepage>
